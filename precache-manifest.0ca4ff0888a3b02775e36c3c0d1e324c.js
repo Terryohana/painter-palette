95f08591e77e1e382291e873da12cf61e8dd9dfd
Zach Holland <zack09holland@gmail.com>
2020-11-13 15:51:16 -0800

    Updates

--- precache-manifest.0ca4ff0888a3b02775e36c3c0d1e324c.js
+++ precache-manifest.0ca4ff0888a3b02775e36c3c0d1e324c.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5b1360bc2f3814279d332d25a6b777cf",
+    "revision": "914b54c7868e792fdacd057acfcd8ecc",
     "url": "/painter-palette/index.html"
   },
   {
-    "revision": "7f49d26fceae96c6c021",
+    "revision": "ef7e1a574bd260a4a644",
     "url": "/painter-palette/static/css/2.ed590494.chunk.css"
   },
   {
-    "revision": "80798c377f13ae640729",
+    "revision": "bd6cb4048770acb33314",
     "url": "/painter-palette/static/css/main.2f289e5e.chunk.css"
   },
   {
-    "revision": "7f49d26fceae96c6c021",
-    "url": "/painter-palette/static/js/2.066e6fd6.chunk.js"
+    "revision": "ef7e1a574bd260a4a644",
+    "url": "/painter-palette/static/js/2.7878ea57.chunk.js"
   },
   {
     "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
-    "url": "/painter-palette/static/js/2.066e6fd6.chunk.js.LICENSE.txt"
+    "url": "/painter-palette/static/js/2.7878ea57.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "80798c377f13ae640729",
-    "url": "/painter-palette/static/js/main.498313c0.chunk.js"
+    "revision": "bd6cb4048770acb33314",
+    "url": "/painter-palette/static/js/main.95522da6.chunk.js"
   },
   {
     "revision": "9a45a6d0fe834721b081",
